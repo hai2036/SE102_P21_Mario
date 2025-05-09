4d19fa6a062ba18e4edd3ba491f090d96edb9675
21522388 <21522388@gm.uit.edu.vn>
2025-05-10 00:04:32 +0700

    New Object Class "Paragoomba" inherited from "Goomba" (Placeholder/No new functionality)

---
+++ Paragoomba.h
@@ -0,0 +1,20 @@
+#pragma once
+#include "Goomba.h"
+
+class CParagoomba : public CGoomba
+{
+protected:
+	virtual void GetBoundingBox(float& left, float& top, float& right, float& bottom);
+	virtual void Update(DWORD dt, vector<LPGAMEOBJECT>* coObjects);
+	virtual void Render();
+
+	virtual int IsCollidable() { return 1; };
+	virtual int IsBlocking() { return 0; }
+	virtual void OnNoCollision(DWORD dt);
+
+	virtual void OnCollisionWith(LPCOLLISIONEVENT e);
+
+public:
+	CParagoomba(float x, float y);
+	virtual void SetState(int state);
+};
