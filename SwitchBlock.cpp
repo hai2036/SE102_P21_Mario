1ab45b8407e6ac5893b551bb121da8abfd880eb4
hai2036 <21522036@gm.uit.edu.vn>
2025-06-03 19:59:57 +0700

    Add SwitchBlock object (P-Switch) switch coin/brick back to original object after 10 seconds and limit the area of effect of switch to 5 UNIT_SIZE from P-Switch each direction

--- SwitchBlock.cpp
+++ SwitchBlock.cpp
@@ -40,6 +40,57 @@ void CSwitchBlock::GetBoundingBox(float& l, float& t, float& r, float& b)
 
 }
 
+void CSwitchBlock::Switch() {
+	vector<vector<LPGAMEOBJECT>> objects = CGame::GetInstance()->GetCurrentScene()->GetObjects();
+	int lastLayer = objects.size() - 1;
+	for (int i = 0; i < objects[lastLayer].size(); i++)
+	{
+		if (dynamic_cast<CBrick*>(objects[lastLayer][i]))
+		{
+			CBrick* brick = dynamic_cast<CBrick*>(objects[lastLayer][i]);
+			if (brick->GetPrizeID() == -1)
+			{
+				float tempX, tempY;
+				brick->GetPosition(tempX, tempY);
+				if (abs(this->x - tempX) <= this->width && abs(this->y - tempY) <= this->height)
+				{
+					brick->Delete();
+					CCoin* coin = new CCoin(tempX, tempY);
+					CGame::GetInstance()->GetCurrentScene()->AddObject(coin, 1);
+				}
+
+			}
+
+		}
+	}
+
+	for (int i = 0; i < objects[1].size(); i++)
+	{
+		if (dynamic_cast<CCoin*>(objects[1][i]))
+		{
+			CCoin* coin = dynamic_cast<CCoin*>(objects[1][i]);
+
+			float tempX, tempY;
+			coin->GetPosition(tempX, tempY);
+			if (abs(this->x - tempX) <= this->width && abs(this->y - tempY) <= this->height)
+			{
+				coin->Delete();
+				CBrick* brick = new CBrick(tempX, tempY, -1);
+				CGame::GetInstance()->GetCurrentScene()->AddObject(brick, lastLayer);
+			}
+		}
+	}
+}
+
+void CSwitchBlock::Update(DWORD dt, vector<LPGAMEOBJECT>* coObjects) {
+	if (state == SWITCH_BLOCK_STATE_PRESSED && timer != -1 && GetTickCount64() - timer > SWITCH_TIME_OUT)
+	{
+		Switch();
+		this->timer = -1;
+	}
+}
+
+
 void CSwitchBlock::SetState(int state)
 {
 	if (this->state == SWITCH_BLOCK_STATE_PRESSED)
@@ -48,24 +99,8 @@ void CSwitchBlock::SetState(int state)
 	}
 	if (state == SWITCH_BLOCK_STATE_PRESSED)
 	{
-		vector<vector<LPGAMEOBJECT>> objects = CGame::GetInstance()->GetCurrentScene()->GetObjects();
-		int lastLayer = objects.size() -1;
-		for (int i = 0; i < objects[lastLayer].size(); i++)
-		{
-			if (dynamic_cast<CBrick*>(objects[lastLayer][i]))
-			{
-				CBrick* brick = dynamic_cast<CBrick*>(objects[lastLayer][i]);
-				if (brick->GetPrizeID() == -1)
-				{
-					float tempX, tempY;
-					brick->GetPosition(tempX, tempY);
-					brick->Delete();
-					CCoin* coin = new CCoin(tempX, tempY);
-					CGame::GetInstance()->GetCurrentScene()->AddObject(coin, 1);
-				}
-				
-			}
-		}
+		Switch();
+		this->timer = GetTickCount64();
 	}
 	CGameObject::SetState(state);
 }
