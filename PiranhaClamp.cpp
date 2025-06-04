a0001131481f6c680b086118bc6d3df5260a5aff
21522388 <21522388@gm.uit.edu.vn>
2025-06-04 11:15:25 +0700

    New Object Class "PiranhaClamp" (Enemy)

---
+++ PiranhaClamp.cpp
@@ -0,0 +1,93 @@
+#include "debug.h"
+#include "PiranhaClamp.h"
+
+#include "Game.h"
+#include "Mario.h"
+#include "Fireball.h"
+
+CPiranhaClamp::CPiranhaClamp(float x, float y) :CGameObject(x, y)
+{
+	y0 = y;
+	y1 = y - PIRANHACLAMP_BBOX_HEIGHT;
+
+	isHostile = false;
+	isRising = false;
+	isOutside = false;
+	rise_start = -1;
+	cooldown_start = -1;
+}
+
+void CPiranhaClamp::GetBoundingBox(float& left, float& top, float& right, float& bottom)
+{
+
+	left = x - PIRANHACLAMP_BBOX_WIDTH / 2;
+	top = y - PIRANHACLAMP_BBOX_HEIGHT / 2;
+	right = left + PIRANHACLAMP_BBOX_WIDTH;
+	bottom = top + PIRANHACLAMP_BBOX_HEIGHT;
+}
+
+void CPiranhaClamp::OnNoCollision(DWORD dt)
+{
+	x += vx * dt;
+	y += vy * dt;
+};
+
+void CPiranhaClamp::OnCollisionWith(LPCOLLISIONEVENT e)
+{
+	if (!e->obj->IsBlocking()) return;
+	if (dynamic_cast<CPiranhaClamp*>(e->obj)) return;
+}
+
+void CPiranhaClamp::Update(DWORD dt, vector<LPGAMEOBJECT>* coObjects)
+{
+	ULONGLONG tick = GetTickCount64();
+	if (!isRising)
+	{
+		if (tick - cooldown_start >= PIRANHACLAMP_RISE_COOLDOWN)
+		{
+			rise_start = tick;
+			isRising = true;
+			isHostile = true;
+		}
+	}
+	else
+	{
+		ULONGLONG timetaken = tick - rise_start;
+		if (timetaken < PIRANHACLAMP_RISE_TIME)
+		{
+			// Su dung cong thuc Lerp (1 - t) * a + t * b
+			float t = (float)timetaken / PIRANHACLAMP_RISE_TIME;
+			if (isOutside)
+			{
+				y = (1 - t) * y1 + t * y0;
+			}
+			else
+			{
+				y = (1 - t) * y0 + t * y1;
+			}
+		}
+		else
+		{
+			y = isOutside ? y0 : y1;
+			isOutside = !isOutside;
+			isHostile = isOutside ? true : false;
+			cooldown_start = tick;
+			isRising = false;
+		}
+	}
+
+	CGameObject::Update(dt, coObjects);
+	CCollision::GetInstance()->Process(this, dt, coObjects);
+}
+
+
+void CPiranhaClamp::Render()
+{
+	CAnimations::GetInstance()->Get(ID_ANI_PIRANHACLAMP_CHOMP)->Render(x, y);
+	RenderBoundingBox();
+}
+
+void CPiranhaClamp::Damage()
+{
+	isDeleted = true;
+}
---
+++ PiranhaClamp.h
@@ -0,0 +1,36 @@
+#pragma once
+#include "AssetIDs.h"
+#include "GameObject.h"
+
+#define PIRANHACLAMP_RISE_TIME 500
+#define PIRANHACLAMP_RISE_COOLDOWN 3000
+
+#define PIRANHACLAMP_BBOX_WIDTH UNIT_SIZE
+#define PIRANHACLAMP_BBOX_HEIGHT UNIT_SIZE * 1.5
+
+class CPiranhaClamp : public CGameObject
+{
+protected:
+	float y0, y1;
+
+	bool isHostile;
+	bool isRising;
+	bool isOutside;
+	ULONGLONG rise_start;
+	ULONGLONG cooldown_start;
+
+	virtual void GetBoundingBox(float& left, float& top, float& right, float& bottom);
+	virtual void Update(DWORD dt, vector<LPGAMEOBJECT>* coObjects);
+	virtual void Render();
+
+	virtual int IsCollidable() { return 1; };
+	virtual int IsBlocking() { return 0; }
+	virtual void OnNoCollision(DWORD dt);
+
+	virtual void OnCollisionWith(LPCOLLISIONEVENT e);
+
+public:
+	CPiranhaClamp(float x, float y);
+	bool GetIsHostile() { return isHostile; }
+	virtual void Damage();
+};
