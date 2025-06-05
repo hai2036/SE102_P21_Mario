82702c4ef7bdda157b0e099f1ce5c7f7c36dcf0e
21522388 <21522388@gm.uit.edu.vn>
2025-06-05 22:47:23 +0700

    Rewritten BoxPlatform Render() for optimization + Added missing shadow sprite rendering

--- BoxPlatform.cpp
+++ BoxPlatform.cpp
@@ -5,264 +5,116 @@
 #include "Textures.h"
 #include "Game.h"
 
-void CBoxPlatform::RenderWhiteBox() {
-	CSprites* sprites = CSprites::GetInstance();
-	float xx = 0;
-	float yy = 0;
-	for (int i = 0; i < this->boxHeight; i++)
-	{
-		yy = y + UNIT_SIZE * i;
-		for (int k = 0; k < this->boxWidth; k++)
-		{
-			xx = x + UNIT_SIZE * k;
-			if (i == 0)
-			{
-				if (k == 0)
-				{
-					sprites->Get(ID_SPRITE_WHITE_BOX1)->Draw(xx, yy);
-				}
-				else if (k == this->boxWidth - 1)
-				{
-					sprites->Get(ID_SPRITE_WHITE_BOX3)->Draw(xx, yy);
-				}
-				else
-				{
-					sprites->Get(ID_SPRITE_WHITE_BOX2)->Draw(xx, yy);
-				}
-			}
-			else if (i == this->boxHeight - 1)
-			{
-				if (k == 0)
-				{
-					sprites->Get(ID_SPRITE_WHITE_BOX7)->Draw(xx, yy);
-				}
-				else if (k == this->boxWidth - 1)
-				{
-					sprites->Get(ID_SPRITE_WHITE_BOX9)->Draw(xx, yy);
-				}
-				else
-				{
-					sprites->Get(ID_SPRITE_WHITE_BOX8)->Draw(xx, yy);
-				}
-			}
-			else
-			{
-				if (k == 0)
-				{
-					sprites->Get(ID_SPRITE_WHITE_BOX4)->Draw(xx, yy);
-				}
-				else if (k == this->boxWidth - 1)
-				{
-					sprites->Get(ID_SPRITE_WHITE_BOX6)->Draw(xx, yy);
-				}
-				else
-				{
-					sprites->Get(ID_SPRITE_WHITE_BOX5)->Draw(xx, yy);
-				}
-			}
-		}
-	}
-}
+void CBoxPlatform::RenderBox(int spriteIds[9]) {
+    CSprites* sprites = CSprites::GetInstance();
 
-void CBoxPlatform::RenderGreenBox() {
-	CSprites* sprites = CSprites::GetInstance();
-	float xx = 0;
-	float yy = 0;
-	for (int i = 0; i < this->boxHeight; i++)
-	{
-		yy = y + UNIT_SIZE * i;
-		for (int k = 0; k < this->boxWidth; k++)
-		{
-			xx = x + UNIT_SIZE * k;
-			if (i == 0)
-			{
-				if (k == 0)
-				{
-					sprites->Get(ID_SPRITE_GREEN_BOX1)->Draw(xx, yy);
-				}
-				else if (k == this->boxWidth - 1)
-				{
-					sprites->Get(ID_SPRITE_GREEN_BOX3)->Draw(xx, yy);
-				}
-				else
-				{
-					sprites->Get(ID_SPRITE_GREEN_BOX2)->Draw(xx, yy);
-				}
-			}
-			else if (i == this->boxHeight - 1)
-			{
-				if (k == 0)
-				{
-					sprites->Get(ID_SPRITE_GREEN_BOX7)->Draw(xx, yy);
-				}
-				else if (k == this->boxWidth - 1)
-				{
-					sprites->Get(ID_SPRITE_GREEN_BOX9)->Draw(xx, yy);
-				}
-				else
-				{
-					sprites->Get(ID_SPRITE_GREEN_BOX8)->Draw(xx, yy);
-				}
-			}
-			else
-			{
-				if (k == 0)
-				{
-					sprites->Get(ID_SPRITE_GREEN_BOX4)->Draw(xx, yy);
-				}
-				else if (k == this->boxWidth - 1)
-				{
-					sprites->Get(ID_SPRITE_GREEN_BOX6)->Draw(xx, yy);
-				}
-				else
-				{
-					sprites->Get(ID_SPRITE_GREEN_BOX5)->Draw(xx, yy);
-				}
-			}
-		}
-	}
-}
+    for (int i = 0; i < boxHeight; ++i) {
+        float yy = y + UNIT_SIZE * i;
 
-void CBoxPlatform::RenderPinkBox() {
-	CSprites* sprites = CSprites::GetInstance();
-	float xx = 0;
-	float yy = 0;
-	for (int i = 0; i < this->boxHeight; i++)
-	{
-		yy = y + UNIT_SIZE * i;
-		for (int k = 0; k < this->boxWidth; k++)
-		{
-			xx = x + UNIT_SIZE * k;
-			if (i == 0)
-			{
-				if (k == 0)
-				{
-					sprites->Get(ID_SPRITE_PINK_BOX1)->Draw(xx, yy);
-				}
-				else if (k == this->boxWidth - 1)
-				{
-					sprites->Get(ID_SPRITE_PINK_BOX3)->Draw(xx, yy);
-				}
-				else
-				{
-					sprites->Get(ID_SPRITE_PINK_BOX2)->Draw(xx, yy);
-				}
-			}
-			else if (i == this->boxHeight - 1)
-			{
-				if (k == 0)
-				{
-					sprites->Get(ID_SPRITE_PINK_BOX7)->Draw(xx, yy);
-				}
-				else if (k == this->boxWidth - 1)
-				{
-					sprites->Get(ID_SPRITE_PINK_BOX9)->Draw(xx, yy);
-				}
-				else
-				{
-					sprites->Get(ID_SPRITE_PINK_BOX8)->Draw(xx, yy);
-				}
-			}
-			else
-			{
-				if (k == 0)
-				{
-					sprites->Get(ID_SPRITE_PINK_BOX4)->Draw(xx, yy);
-				}
-				else if (k == this->boxWidth - 1)
-				{
-					sprites->Get(ID_SPRITE_PINK_BOX6)->Draw(xx, yy);
-				}
-				else
-				{
-					sprites->Get(ID_SPRITE_PINK_BOX5)->Draw(xx, yy);
-				}
-			}
-		}
-	}
-}
+        for (int k = 0; k < boxWidth; ++k) {
+            float xx = x + UNIT_SIZE * k;
+            int spriteIndex = 4; //Center
 
-void CBoxPlatform::RenderBlueBox() {
-	CSprites* sprites = CSprites::GetInstance();
-	float xx = 0;
-	float yy = 0;
-	for (int i = 0; i < this->boxHeight; i++)
-	{
-		yy = y + UNIT_SIZE * i;
-		for (int k = 0; k < this->boxWidth; k++)
-		{
-			xx = x + UNIT_SIZE * k;
-			if (i == 0)
-			{
-				if (k == 0)
-				{
-					sprites->Get(ID_SPRITE_BLUE_BOX1)->Draw(xx, yy);
-				}
-				else if (k == this->boxWidth - 1)
-				{
-					sprites->Get(ID_SPRITE_BLUE_BOX3)->Draw(xx, yy);
-				}
-				else
-				{
-					sprites->Get(ID_SPRITE_BLUE_BOX2)->Draw(xx, yy);
-				}
-			}
-			else if (i == this->boxHeight - 1)
-			{
-				if (k == 0)
-				{
-					sprites->Get(ID_SPRITE_BLUE_BOX7)->Draw(xx, yy);
-				}
-				else if (k == this->boxWidth - 1)
-				{
-					sprites->Get(ID_SPRITE_BLUE_BOX9)->Draw(xx, yy);
-				}
-				else
-				{
-					sprites->Get(ID_SPRITE_BLUE_BOX8)->Draw(xx, yy);
-				}
-			}
-			else
-			{
-				if (k == 0)
-				{
-					sprites->Get(ID_SPRITE_BLUE_BOX4)->Draw(xx, yy);
-				}
-				else if (k == this->boxWidth - 1)
-				{
-					sprites->Get(ID_SPRITE_BLUE_BOX6)->Draw(xx, yy);
-				}
-				else
-				{
-					sprites->Get(ID_SPRITE_BLUE_BOX5)->Draw(xx, yy);
-				}
-			}
-		}
-	}
+            if (i == 0) {
+                if (k == 0)
+                    spriteIndex = 0; //TopLeft
+                else if (k == boxWidth - 1) {
+                    spriteIndex = 2; //TopRight
+                    sprites->Get(ID_SPRITE_BOX_PLATFORM_SHADOW_CORNER)->Draw(xx + UNIT_SIZE, yy); //TopRightShadow
+                }
+                else
+                    spriteIndex = 1; //TopMid
+            }
+            else if (i == boxHeight - 1) {
+                if (k == 0)
+                    spriteIndex = 6; //BottomLeft
+                else if (k == boxWidth - 1) {
+                    spriteIndex = 8; //BottomRight
+                    sprites->Get(ID_SPRITE_BOX_PLATFORM_SHADOW_EDGE)->Draw(xx + UNIT_SIZE, yy); //BottomRightShadow
+                }
+                else
+                    spriteIndex = 7; //BottomMid
+            }
+            else {
+                if (k == 0)
+                    spriteIndex = 3; //EdgeLeft
+                else if (k == boxWidth - 1) {
+                    spriteIndex = 5; //EdgeRight
+                    sprites->Get(ID_SPRITE_BOX_PLATFORM_SHADOW_EDGE)->Draw(xx + UNIT_SIZE, yy); //EdgeRightShadow
+                }
+            }
+
+            sprites->Get(spriteIds[spriteIndex])->Draw(xx, yy);
+        }
+    }
 }
 
 void CBoxPlatform::Render()
 {
-	
-	switch (this->color)
-	{
-	case COLOR_WHITE:
-		RenderWhiteBox();
-		break;
-	case COLOR_GREEN:
-		RenderGreenBox();
-		break;
-	case COLOR_PINK:
-		RenderPinkBox();
-		break;
-	case COLOR_BLUE:
-		RenderBlueBox();
-		break;
+    int whiteBoxSprites[9] = {
+        ID_SPRITE_BOX_PLATFORM_WHITE_1,
+        ID_SPRITE_BOX_PLATFORM_WHITE_2,
+        ID_SPRITE_BOX_PLATFORM_WHITE_3,
+        ID_SPRITE_BOX_PLATFORM_WHITE_4,
+        ID_SPRITE_BOX_PLATFORM_WHITE_5,
+        ID_SPRITE_BOX_PLATFORM_WHITE_6,
+        ID_SPRITE_BOX_PLATFORM_WHITE_7,
+        ID_SPRITE_BOX_PLATFORM_WHITE_8,
+        ID_SPRITE_BOX_PLATFORM_WHITE_9
+    };
 
-	default:
-		break;
-	}
-	
+    int greenBoxSprites[9] = {
+        ID_SPRITE_BOX_PLATFORM_GREEN_1,
+        ID_SPRITE_BOX_PLATFORM_GREEN_2,
+        ID_SPRITE_BOX_PLATFORM_GREEN_3,
+        ID_SPRITE_BOX_PLATFORM_GREEN_4,
+        ID_SPRITE_BOX_PLATFORM_GREEN_5,
+        ID_SPRITE_BOX_PLATFORM_GREEN_6,
+        ID_SPRITE_BOX_PLATFORM_GREEN_7,
+        ID_SPRITE_BOX_PLATFORM_GREEN_8,
+        ID_SPRITE_BOX_PLATFORM_GREEN_9
+    };
+
+    int pinkBoxSprites[9] = {
+        ID_SPRITE_BOX_PLATFORM_PINK_1,
+        ID_SPRITE_BOX_PLATFORM_PINK_2,
+        ID_SPRITE_BOX_PLATFORM_PINK_3,
+        ID_SPRITE_BOX_PLATFORM_PINK_4,
+        ID_SPRITE_BOX_PLATFORM_PINK_5,
+        ID_SPRITE_BOX_PLATFORM_PINK_6,
+        ID_SPRITE_BOX_PLATFORM_PINK_7,
+        ID_SPRITE_BOX_PLATFORM_PINK_8,
+        ID_SPRITE_BOX_PLATFORM_PINK_9
+    };
+
+    int blueBoxSprites[9] = {
+        ID_SPRITE_BOX_PLATFORM_BLUE_1,
+        ID_SPRITE_BOX_PLATFORM_BLUE_2,
+        ID_SPRITE_BOX_PLATFORM_BLUE_3,
+        ID_SPRITE_BOX_PLATFORM_BLUE_4,
+        ID_SPRITE_BOX_PLATFORM_BLUE_5,
+        ID_SPRITE_BOX_PLATFORM_BLUE_6,
+        ID_SPRITE_BOX_PLATFORM_BLUE_7,
+        ID_SPRITE_BOX_PLATFORM_BLUE_8,
+        ID_SPRITE_BOX_PLATFORM_BLUE_9
+    };
+
+    switch (color) {
+    case COLOR_WHITE:
+        RenderBox(whiteBoxSprites);
+        break;
+    case COLOR_GREEN:
+        RenderBox(greenBoxSprites);
+        break;
+    case COLOR_PINK:
+        RenderBox(pinkBoxSprites);
+        break;
+    case COLOR_BLUE:
+        RenderBox(blueBoxSprites);
+        break;
+    default:
+        break;
+    }
 	//RenderBoundingBox();
 }
 
--- BoxPlatform.h
+++ BoxPlatform.h
@@ -1,58 +1,17 @@
 #pragma once
+#include "AssetIDs.h"
 #include "GameObject.h"
-
 
 #define COLOR_WHITE 0
 #define COLOR_GREEN 1
 #define COLOR_PINK 2
 #define COLOR_BLUE 3
 
-#define ID_SPRITE_WHITE_BOX1 70001
-#define ID_SPRITE_WHITE_BOX2 70002
-#define ID_SPRITE_WHITE_BOX3 70003
-#define ID_SPRITE_WHITE_BOX4 70004
-#define ID_SPRITE_WHITE_BOX5 70005
-#define ID_SPRITE_WHITE_BOX6 70006
-#define ID_SPRITE_WHITE_BOX7 70007
-#define ID_SPRITE_WHITE_BOX8 70008
-#define ID_SPRITE_WHITE_BOX9 70009
 
-#define ID_SPRITE_GREEN_BOX1 71001
-#define ID_SPRITE_GREEN_BOX2 71002
-#define ID_SPRITE_GREEN_BOX3 71003
-#define ID_SPRITE_GREEN_BOX4 71004
-#define ID_SPRITE_GREEN_BOX5 71005
-#define ID_SPRITE_GREEN_BOX6 71006
-#define ID_SPRITE_GREEN_BOX7 71007
-#define ID_SPRITE_GREEN_BOX8 71008
-#define ID_SPRITE_GREEN_BOX9 71009
-
-#define ID_SPRITE_PINK_BOX1 72001
-#define ID_SPRITE_PINK_BOX2 72002
-#define ID_SPRITE_PINK_BOX3 72003
-#define ID_SPRITE_PINK_BOX4 72004
-#define ID_SPRITE_PINK_BOX5 72005
-#define ID_SPRITE_PINK_BOX6 72006
-#define ID_SPRITE_PINK_BOX7 72007
-#define ID_SPRITE_PINK_BOX8 72008
-#define ID_SPRITE_PINK_BOX9 72009
-
-#define ID_SPRITE_BLUE_BOX1 73001
-#define ID_SPRITE_BLUE_BOX2 73002
-#define ID_SPRITE_BLUE_BOX3 73003
-#define ID_SPRITE_BLUE_BOX4 73004
-#define ID_SPRITE_BLUE_BOX5 73005
-#define ID_SPRITE_BLUE_BOX6 73006
-#define ID_SPRITE_BLUE_BOX7 73007
-#define ID_SPRITE_BLUE_BOX8 73008
-#define ID_SPRITE_BLUE_BOX9 73009
 
 class CBoxPlatform : public CGameObject
 {
-	void RenderWhiteBox();
-	void RenderGreenBox();
-	void RenderPinkBox();
-	void RenderBlueBox();
+	void RenderBox(int spriteIds[9]);
 protected:
 	int boxWidth;
 	int boxHeight;
@@ -64,7 +23,7 @@ public:
 		this->boxHeight = boxHeight;
 		this->color = color;
 	}
-
+	
 	void Render();
 	void Update(DWORD dt) {}
 	void GetBoundingBox(float& l, float& t, float& r, float& b);
